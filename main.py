bc4f16ceb8833c703d3cd003bbb32311b40e7528
RechHAYMI <sasha.rumyantsev.14@bk.ru>
2026-04-21 20:16:22 +0300

    Added Reply Keyboard and buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, ReplyKeyboardMarkup, KeyboardButton 
 from aiogram.filters import Command
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -9,7 +9,9 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("Приветик! Это мой первый бот.")
+    my_button = KeyboardButton(text="Start")
+    my_kb = ReplyKeyboardMarkup(keyboard=[[my_button]], resize_keyboard=True)
+    await message.answer("Приветик! Это мой первый бот.", reply_markup=my_kb)
 @dp.message()
 async def copy_message(message: types.Message):
     await message.answer(message.text)

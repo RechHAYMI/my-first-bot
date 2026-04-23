699a13696b51bfb5e2b7e236868452cca2124874
RechHAYMI <sasha.rumyantsev.14@bk.ru>
2026-04-23 23:33:49 +0300

    Day 24: Added sub-menu and fixed lower case logic

--- main.py
+++ main.py
@@ -9,11 +9,26 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    my_button = KeyboardButton(text="Start")
-    my_kb = ReplyKeyboardMarkup(keyboard=[[my_button]], resize_keyboard=True)
-    await message.answer("Погнали.", reply_markup=my_kb)
+    button_start = KeyboardButton(text="Start")
+    button_info = KeyboardButton(text="Info")
+    button_settings = KeyboardButton(text="Settings")
+    my_kb = ReplyKeyboardMarkup(keyboard=[[button_start, button_info], [button_settings]], resize_keyboard=True)
+    await message.answer("Привет! Нажми на кнопку ниже, чтобы начать.", reply_markup=my_kb)
 @dp.message()
-async def handle_all
+async def handle_all(message: types.Message):
+    if message.text == "Start":
+        await message.answer("Погнали.")
+    elif message.text.lower() == "info":
+        await message.answer("Привет, это мой первый бот на пайтон")
+    elif message.text.lower() == "settings":
+        btn_name = KeyboardButton(text="Изменить имя")
+        btn_back = KeyboardButton(text="Назад")
+        settings_kb = ReplyKeyboardMarkup(keyboard=[[btn_name, btn_back]], resize_keyboard=True)
+        await message.answer("Вы в настройках что изменим?", reply_markup=settings_kb)
+    elif message.text.lower() == "назад":
+        await start(message)
+    else:
+        await message.answer("Я тебя не понимаю.")
 async def main():
     await dp.start_polling(bot)
 asyncio.run(main())

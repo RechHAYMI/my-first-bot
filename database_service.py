23f295a50f3f7085e928457c55be5bda5199c915
RechHAYMI <sasha.rumyantsev.14@bk.ru>
2026-06-28 11:39:19 +0300

    refactor: integrate DatabaseLayer into ShadowMiddleware

--- database_service.py
+++ database_service.py
@@ -1,4 +1,4 @@
-#если кто то читает,ниже описание для самого меня
+##если кто то читает,ниже описание для самого меня
 class DatabaseLayer(): #создаем класс что создать коробку
     def __init__(self, pool): #инит конструктор
         self.pool = pool # указываем что пул это селф пул тем самым помещаем его в коробку
@@ -6,8 +6,7 @@ class DatabaseLayer(): #создаем класс что создать коро
         await self.pool.execute( # sql запрос
         "INSERT INTO users (telegram_id, username, first_name) VALUES ($1, $2, $3) ON CONFLICT (telegram_id) DO NOTHING",
         telegram_id, username, first_name
-    )
-
+        )
     async def db_check_user(self, telegram_id): # тоже копируем функцию и ниже запросы, но только пул в коробке
-        result_id = await self.pool.fetchval("SELECT 1 FROM users WHERE telegram_id = $1", telegram_id) 
-        return result_id is not None 
+        result_id = await self.pool.fetchval("SELECT 1 FROM users WHERE telegram_id = $1", telegram_id)
+        return result_id is not None
--- middlewares/main_middleware.py
+++ middlewares/main_middleware.py
@@ -1,15 +1,16 @@
 #если также ктото читает, это заметка для меня, я писал все сам, но забыл, поэтому обьясню
-from database import db_add_user #ниже иморты всякие
+from database_service import DatabaseLayer
 from aiogram import BaseMiddleware
 from typing import Dict, Any, Callable
 from config import ADMIN_ID
 
 class ShadowMiddleware(BaseMiddleware): #создаю класс на базе BaseMiddleware
     async def __call__(self, handler, event, data): # это дверь, через которую проходит абсолютно любое действие пользователя
-        pool = data.get("pool") # обозначаю что такое пул
+        pool = data.get("pool")
+        db = DatabaseLayer(pool)
+        data["db"] = db
         user = event.from_user # Обозначаю что такое user
         if user is not None and pool: # проверяю есть ли юзер и было ли взаимодействие с пулом
-            await db_add_user(pool, user.id, user.username, user.first_name) # если он не зареган то добавляю в дб
+            await db.add_user(user.id, user.username, user.first_name) # если он не зареган то добавляю в дб
         data["is_admin"] = (user.id == ADMIN_ID) # если его телеграм айди равен админ айди то выдаю админку
-        data["db"] = DatabaseLayer(pool) # обозначаю что db это DatabaseLayer то есть коробка с пулом
-        return await handler(event, data) #Middleware говорит боту: «Я всё проверил, коробку в сумку положил, теперь пропускай событие дальше к хэндлеру, вот тебе обновленная сумка data».
+        return await handler(event, data) 
